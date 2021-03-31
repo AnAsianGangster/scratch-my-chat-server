e0ca674b3c061ca4e94d7d636e9bbcddfb7a0d3e
AnAsianGangster <yangaobo1996@gmail.com>
2021-04-01 00:58:01 +0800

    [feat] initial deployment attempt

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
-const cors = require('cors');
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 
 const app = express();
-app.use(cors());
 const server = http.createServer(app);
 // TODO change the cors options
 const io = socketio(server, {
